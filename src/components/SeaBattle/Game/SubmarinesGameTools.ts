9d1e61d42030880681c4448fb5e90a53fa511ff3
roi avidar <roiavidar@gmail.com>
2020-08-27 14:08:13 +0300

    add game tools

--- src/components/SeaBattle/Game/SubmarinesGameTools.ts
+++ src/components/SeaBattle/Game/SubmarinesGameTools.ts
@@ -15,40 +15,40 @@ export const getSubmarines: () => SubmarineModel[] = function getSubmarines() {
             vertical: false,
             size: 4
         },
-        // {
-        //     id: 2,
-        //     vertical: false,
-        //     size: 3
-        // },
-        // {
-        //     id: 3,
-        //     vertical: false,
-        //     size: 2
-        // },
-        // {
-        //     id: 4,
-        //     vertical: false,
-        //     size: 2
-        // },
-        // {
-        //     id: 5,
-        //     vertical: false,
-        //     size: 1
-        // },
-        // {
-        //     id: 6,
-        //     vertical: false,
-        //     size: 1
-        // },
-        // {
-        //     id: 7,
-        //     vertical: false,
-        //     size: 1
-        // },
-        // {
-        //     id: 8,
-        //     vertical: false,
-        //     size: 1
-        // }
+        {
+            id: 2,
+            vertical: false,
+            size: 3
+        },
+        {
+            id: 3,
+            vertical: false,
+            size: 2
+        },
+        {
+            id: 4,
+            vertical: false,
+            size: 2
+        },
+        {
+            id: 5,
+            vertical: false,
+            size: 1
+        },
+        {
+            id: 6,
+            vertical: false,
+            size: 1
+        },
+        {
+            id: 7,
+            vertical: false,
+            size: 1
+        },
+        {
+            id: 8,
+            vertical: false,
+            size: 1
+        }
     ]
 }
